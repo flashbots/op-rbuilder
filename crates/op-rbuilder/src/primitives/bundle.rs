5214bb9763bf51c6063fea56e306c84038287110
Ferran Borreguero <ferran.borreguero@gmail.com>
2025-05-22 11:09:43 +0100

    Rename bundle fields

--- crates/op-rbuilder/src/primitives/bundle.rs
+++ crates/op-rbuilder/src/primitives/bundle.rs
@@ -3,7 +3,10 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Bundle {
+    #[serde(rename = "txs")]
     pub transactions: Vec<Bytes>,
+
+    #[serde(rename = "maxBlockNumber")]
     pub block_number_max: Option<u64>,
 }
 
