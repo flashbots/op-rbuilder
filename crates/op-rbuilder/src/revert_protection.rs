98b97bec8b3831532d037ed0eb27ead62a2e1e71
Ferran Borreguero <ferran.borreguero@gmail.com>
2025-05-22 15:03:15 +0100

    Handle unwraps

--- crates/op-rbuilder/src/revert_protection.rs
+++ crates/op-rbuilder/src/revert_protection.rs
@@ -7,10 +7,10 @@ use jsonrpsee::{
     core::{async_trait, RpcResult},
     proc_macros::rpc,
 };
-use reth::rpc::result::rpc_err;
 use reth_optimism_txpool::{conditional::MaybeConditionalTransaction, OpPooledTransaction};
 use reth_provider::StateProviderFactory;
 use reth_rpc_eth_types::utils::recover_raw_transaction;
+use reth_rpc_eth_types::EthApiError;
 use reth_transaction_pool::{PoolTransaction, TransactionOrigin, TransactionPool};
 
 // Namespace overrides for revert protection support
@@ -39,32 +39,41 @@ where
     Provider: StateProviderFactory + Send + Sync + Clone + 'static,
 {
     async fn send_bundle(&self, mut bundle: Bundle) -> RpcResult<B256> {
-        let last_block_number = self.provider.best_block_number().unwrap(); // FIXME: do not unwrap
+        let last_block_number = self
+            .provider
+            .best_block_number()
+            .map_err(|_e| EthApiError::InternalEthError)?;
 
         // Only one transaction in the bundle is expected
         let bundle_transaction = match bundle.transactions.len() {
             0 => {
-                return Err(rpc_err_invalid_params(
-                    "bundle must contain at least one transaction",
-                ));
+                return Err(EthApiError::InvalidParams(
+                    "bundle must contain at least one transaction".into(),
+                )
+                .into());
             }
             1 => bundle.transactions[0].clone(),
             _ => {
-                return Err(rpc_err_invalid_params(
-                    "bundle must contain exactly one transaction",
-                ));
+                return Err(EthApiError::InvalidParams(
+                    "bundle must contain exactly one transaction".into(),
+                )
+                .into());
             }
         };
 
         if let Some(block_number_max) = bundle.block_number_max {
             // The max block cannot be a past block
             if block_number_max <= last_block_number {
-                return Err(rpc_err_invalid_params("block_number_max is a past block"));
+                return Err(
+                    EthApiError::InvalidParams("block_number_max is a past block".into()).into(),
+                );
             }
 
             // Validate that it is not greater than the max_block_range
             if block_number_max > last_block_number + MAX_BLOCK_RANGE_BLOCKS {
-                return Err(rpc_err_invalid_params("block_number_max is too high"));
+                return Err(
+                    EthApiError::InvalidParams("block_number_max is too high".into()).into(),
+                );
             }
         } else {
             // If no upper bound is set, use the maximum block range
@@ -82,12 +91,8 @@ where
             .pool
             .add_transaction(TransactionOrigin::Local, pool_transaction)
             .await
-            .unwrap(); // TODO: FIX THIS
+            .map_err(|e| EthApiError::from(e))?;
 
         Ok(hash)
     }
-}
-
-fn rpc_err_invalid_params(msg: &str) -> jsonrpsee_types::ErrorObjectOwned {
-    rpc_err(jsonrpsee_types::error::INVALID_PARAMS_CODE, msg, None)
 }
