a041b4b8c732a8abf4ee2e63a7a7dfe0accf2ccb
Denis Kolodin <deniskolodin@gmail.com>
2025-06-19 15:14:17 +0200

    feat: add the BestTransactionsExecutor scope

--- crates/op-rbuilder/src/builders/context.rs
+++ crates/op-rbuilder/src/builders/context.rs
@@ -6,17 +6,19 @@ use alloy_op_evm::block::receipt_builder::OpReceiptBuilder;
 use alloy_primitives::{Address, Bytes, TxKind, U256};
 use alloy_rpc_types_eth::Withdrawals;
 use core::fmt::Debug;
+use derive_more::Deref;
 use op_alloy_consensus::{OpDepositReceipt, OpTypedTransaction};
 use op_revm::OpSpecId;
 use reth::payload::PayloadBuilderAttributes;
 use reth_basic_payload_builder::PayloadConfig;
 use reth_chainspec::{EthChainSpec, EthereumHardforks};
 use reth_evm::{
-    eth::receipt_builder::ReceiptBuilderCtx, ConfigureEvm, Evm, EvmEnv, EvmError, InvalidTxError,
+    eth::receipt_builder::ReceiptBuilderCtx, precompiles::PrecompilesMap, ConfigureEvm, Evm,
+    EvmEnv, EvmError, InvalidTxError,
 };
 use reth_node_api::PayloadBuilderError;
 use reth_optimism_chainspec::OpChainSpec;
-use reth_optimism_evm::{OpEvmConfig, OpNextBlockEnvAttributes};
+use reth_optimism_evm::{OpEvm, OpEvmConfig, OpNextBlockEnvAttributes};
 use reth_optimism_forks::OpHardforks;
 use reth_optimism_node::OpPayloadBuilderAttributes;
 use reth_optimism_payload_builder::{config::OpDAConfig, error::OpPayloadBuilderError};
@@ -33,6 +35,7 @@ use reth_provider::ProviderError;
 use reth_revm::{context::Block, State};
 use reth_transaction_pool::{BestTransactionsAttributes, PoolTransaction};
 use revm::{context::result::ResultAndState, Database, DatabaseCommit};
+use revm_inspector::NoOpInspector;
 use std::{sync::Arc, time::Instant};
 use tokio_util::sync::CancellationToken;
 use tracing::{debug, info, trace, warn};
@@ -191,6 +194,13 @@ impl OpPayloadBuilderCtx {
     pub fn builder_signer(&self) -> Option<Signer> {
         self.builder_signer
     }
+}
+
+pub enum StepStatus {
+    // TODO: Add a reason here
+    Continue,
+    Interrupt,
+    Failed(PayloadBuilderError),
 }
 
 impl OpPayloadBuilderCtx {
@@ -687,4 +697,238 @@ where
     let builder_tx = signer.sign_tx(tx).map_err(PayloadBuilderError::other)?;
 
     Ok(builder_tx)
+}
+
+#[derive(Deref)]
+pub struct BestTransactionsExecutor<'a, DB, E, TXS>
+where
+    DB: Database<Error = ProviderError>,
+    E: Debug + Default,
+    TXS: PayloadTxsBounds,
+{
+    #[deref]
+    ctx: &'a OpPayloadBuilderCtx,
+    info: &'a mut ExecutionInfo<E>,
+    // db: &'a mut State<DB>,
+    best_txs: TXS,
+    block_gas_limit: u64,
+    block_da_limit: Option<u64>,
+
+    evm: OpEvm<&'a mut State<DB>, NoOpInspector, PrecompilesMap>,
+    base_fee: u64,
+    tx_da_limit: Option<u64>,
+    block_attr: BlockConditionalAttributes,
+
+    execute_txs_start_time: Instant,
+    num_txs_considered: usize,
+    num_txs_simulated: usize,
+    num_txs_simulated_success: usize,
+    num_txs_simulated_fail: usize,
+    num_bundles_reverted: usize,
+}
+
+impl<'a, DB, E, TXS> BestTransactionsExecutor<'a, DB, E, TXS>
+where
+    DB: Database<Error = ProviderError>,
+    E: Debug + Default,
+    TXS: PayloadTxsBounds,
+{
+    pub fn new(
+        ctx: &'a OpPayloadBuilderCtx,
+        info: &'a mut ExecutionInfo<E>,
+        db: &'a mut State<DB>,
+        best_txs: TXS,
+        block_gas_limit: u64,
+        block_da_limit: Option<u64>,
+    ) -> Self {
+        let start = Instant::now();
+        let base_fee = ctx.base_fee();
+        let tx_da_limit = ctx.da_config.max_da_tx_size();
+        let env = ctx.evm_env.clone();
+        let evm = ctx.evm_config.evm_with_env(&mut *db, env);
+
+        let block_attr = BlockConditionalAttributes {
+            number: ctx.block_number(),
+            timestamp: ctx.attributes().timestamp(),
+        };
+        Self {
+            ctx,
+            info,
+            // db,
+            best_txs,
+            block_gas_limit,
+            block_da_limit,
+
+            evm,
+            base_fee,
+            tx_da_limit,
+            block_attr,
+
+            execute_txs_start_time: start,
+            num_txs_considered: 0,
+            num_txs_simulated: 0,
+            num_txs_simulated_success: 0,
+            num_txs_simulated_fail: 0,
+            num_bundles_reverted: 0,
+        }
+    }
+
+    pub fn execute_best_one(&mut self, tx: TXS::Transaction) -> StepStatus {
+        let interop = tx.interop_deadline();
+        let reverted_hashes = tx.reverted_hashes().clone();
+        let conditional = tx.conditional().cloned();
+
+        let tx_da_size = tx.estimated_da_size();
+        let tx = tx.into_consensus();
+        let tx_hash = tx.tx_hash();
+
+        // exclude reverting transaction if:
+        // - the transaction comes from a bundle (is_some) and the hash **is not** in reverted hashes
+        // Note that we need to use the Option to signal whether the transaction comes from a bundle,
+        // otherwise, we would exclude all transactions that are not in the reverted hashes.
+        let is_bundle_tx = reverted_hashes.is_some();
+        let exclude_reverting_txs = is_bundle_tx && !reverted_hashes.unwrap().contains(&tx_hash);
+
+        let log_txn = |result: TxnExecutionResult| {
+            debug!(
+                target: "payload_builder",
+                message = "Considering transaction",
+                tx_hash = ?tx_hash,
+                tx_da_size = ?tx_da_size,
+                exclude_reverting_txs = ?exclude_reverting_txs,
+                result = %result,
+            );
+        };
+
+        self.num_txs_considered += 1;
+
+        if let Some(conditional) = conditional {
+            // TODO: ideally we should get this from the txpool stream
+            if !conditional.matches_block_attributes(&self.block_attr) {
+                self.best_txs.mark_invalid(tx.signer(), tx.nonce());
+                return StepStatus::Continue;
+            }
+        }
+
+        // TODO: remove this condition and feature once we are comfortable enabling interop for everything
+        if cfg!(feature = "interop") {
+            // We skip invalid cross chain txs, they would be removed on the next block update in
+            // the maintenance job
+            if let Some(interop) = interop {
+                if !is_valid_interop(interop, self.config.attributes.timestamp()) {
+                    log_txn(TxnExecutionResult::InteropFailed);
+                    self.best_txs.mark_invalid(tx.signer(), tx.nonce());
+                    return StepStatus::Continue;
+                }
+            }
+        }
+
+        // ensure we still have capacity for this transaction
+        if let Err(result) = self.info.is_tx_over_limits(
+            tx_da_size,
+            self.block_gas_limit,
+            self.tx_da_limit,
+            self.block_da_limit,
+            tx.gas_limit(),
+        ) {
+            // we can't fit this transaction into the block, so we need to mark it as
+            // invalid which also removes all dependent transaction from
+            // the iterator before we can continue
+            log_txn(result);
+            self.best_txs.mark_invalid(tx.signer(), tx.nonce());
+            return StepStatus::Continue;
+        }
+
+        // A sequencer's block should never contain blob or deposit transactions from the pool.
+        if tx.is_eip4844() || tx.is_deposit() {
+            log_txn(TxnExecutionResult::SequencerTransaction);
+            self.best_txs.mark_invalid(tx.signer(), tx.nonce());
+            return StepStatus::Continue;
+        }
+
+        // check if the job was cancelled, if so we can exit early
+        if self.cancel.is_cancelled() {
+            return StepStatus::Interrupt;
+        }
+
+        let tx_simulation_start_time = Instant::now();
+        let ResultAndState { result, state } = match self.evm.transact(&tx) {
+            Ok(res) => res,
+            Err(err) => {
+                if let Some(err) = err.as_invalid_tx_err() {
+                    if err.is_nonce_too_low() {
+                        // if the nonce is too low, we can skip this transaction
+                        log_txn(TxnExecutionResult::NonceTooLow);
+                        trace!(target: "payload_builder", %err, ?tx, "skipping nonce too low transaction");
+                    } else {
+                        // if the transaction is invalid, we can skip it and all of its
+                        // descendants
+                        log_txn(TxnExecutionResult::InternalError(err.clone()));
+                        trace!(target: "payload_builder", %err, ?tx, "skipping invalid transaction and its descendants");
+                        self.best_txs.mark_invalid(tx.signer(), tx.nonce());
+                    }
+
+                    return StepStatus::Continue;
+                }
+                // this is an error that we should treat as fatal for this attempt
+                log_txn(TxnExecutionResult::EvmError);
+                return StepStatus::Failed(PayloadBuilderError::evm(err));
+            }
+        };
+
+        self.metrics
+            .tx_simulation_duration
+            .record(tx_simulation_start_time.elapsed());
+        self.metrics.tx_byte_size.record(tx.inner().size() as f64);
+        self.num_txs_simulated += 1;
+        if result.is_success() {
+            log_txn(TxnExecutionResult::Success);
+            self.num_txs_simulated_success += 1;
+        } else {
+            self.num_txs_simulated_fail += 1;
+            if is_bundle_tx {
+                self.num_bundles_reverted += 1;
+            }
+            if exclude_reverting_txs {
+                log_txn(TxnExecutionResult::RevertedAndExcluded);
+                info!(target: "payload_builder", tx_hash = ?tx.tx_hash(), "skipping reverted transaction");
+                self.best_txs.mark_invalid(tx.signer(), tx.nonce());
+                return StepStatus::Continue;
+            } else {
+                log_txn(TxnExecutionResult::Reverted);
+            }
+        }
+
+        // add gas used by the transaction to cumulative gas used, before creating the
+        // receipt
+        let gas_used = result.gas_used();
+        self.info.cumulative_gas_used += gas_used;
+        // record tx da size
+        self.info.cumulative_da_bytes_used += tx_da_size;
+
+        // Push transaction changeset and calculate header bloom filter for receipt.
+        let ctx = ReceiptBuilderCtx {
+            tx: tx.inner(),
+            evm: &self.evm,
+            result,
+            state: &state,
+            cumulative_gas_used: self.info.cumulative_gas_used,
+        };
+        self.info.receipts.push(self.build_receipt(ctx, None));
+
+        // commit changes
+        self.evm.db_mut().commit(state);
+
+        // update add to total fees
+        let miner_fee = tx
+            .effective_tip_per_gas(self.base_fee)
+            .expect("fee is always valid; execution succeeded");
+        self.info.total_fees += U256::from(miner_fee) * U256::from(gas_used);
+
+        // append sender and transaction to the respective lists
+        self.info.executed_senders.push(tx.signer());
+        self.info.executed_transactions.push(tx.into_inner());
+
+        StepStatus::Continue
+    }
 }
