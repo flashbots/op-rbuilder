18111e99298b3f3f86eedf78c73c0e44244baa78
Niven <sieniven@gmail.com>
2025-11-27 21:40:24 +0800

    Fixes

--- crates/op-rbuilder/src/builders/flashblocks/payload.rs
+++ crates/op-rbuilder/src/builders/flashblocks/payload.rs
@@ -30,7 +30,7 @@ use reth_optimism_evm::{OpEvmConfig, OpNextBlockEnvAttributes};
 use reth_optimism_forks::OpHardforks;
 use reth_optimism_node::{OpBuiltPayload, OpPayloadBuilderAttributes};
 use reth_optimism_primitives::{OpPrimitives, OpReceipt, OpTransactionSigned};
-use reth_payload_primitives::BuiltPayloadExecutedBlock;
+use reth_payload_primitives::{BuiltPayload, BuiltPayloadExecutedBlock};
 use reth_payload_util::BestPayloadTransactions;
 use reth_primitives_traits::RecoveredBlock;
 use reth_provider::{
@@ -145,9 +145,12 @@ pub(super) struct OpPayloadBuilder<Pool, Client, BuilderTx> {
     pub pool: Pool,
     /// Node client
     pub client: Client,
-    /// Sender for sending built payloads to [`PayloadHandler`],
-    /// which broadcasts outgoing payloads via p2p.
-    pub payload_tx: mpsc::Sender<OpBuiltPayload>,
+    /// Sender for sending built flashblock payloads to [`PayloadHandler`],
+    /// which broadcasts outgoing flashblock payloads via p2p.
+    pub built_fb_payload_tx: mpsc::Sender<OpBuiltPayload>,
+    /// Sender for sending built full block payloads to [`PayloadHandler`],
+    /// which updates the engine tree state.
+    pub built_payload_tx: mpsc::Sender<OpBuiltPayload>,
     /// WebSocket publisher for broadcasting flashblocks
     /// to all connected subscribers.
     pub ws_pub: Arc<WebSocketPublisher>,
@@ -170,7 +173,8 @@ impl<Pool, Client, BuilderTx> OpPayloadBuilder<Pool, Client, BuilderTx> {
         client: Client,
         config: BuilderConfig<FlashblocksConfig>,
         builder_tx: BuilderTx,
-        payload_tx: mpsc::Sender<OpBuiltPayload>,
+        built_fb_payload_tx: mpsc::Sender<OpBuiltPayload>,
+        built_payload_tx: mpsc::Sender<OpBuiltPayload>,
         ws_pub: Arc<WebSocketPublisher>,
         metrics: Arc<OpRBuilderMetrics>,
     ) -> Self {
@@ -179,7 +183,8 @@ impl<Pool, Client, BuilderTx> OpPayloadBuilder<Pool, Client, BuilderTx> {
             evm_config,
             pool,
             client,
-            payload_tx,
+            built_fb_payload_tx,
+            built_payload_tx,
             ws_pub,
             config,
             metrics,
@@ -297,7 +302,7 @@ where
     async fn build_payload(
         &self,
         args: BuildArguments<OpPayloadBuilderAttributes<OpTransactionSigned>, OpBuiltPayload>,
-        best_payload: BlockCell<OpBuiltPayload>,
+        resolve_payload: BlockCell<OpBuiltPayload>,
     ) -> Result<(), PayloadBuilderError> {
         let block_build_start_time = Instant::now();
         let BuildArguments {
@@ -366,18 +371,13 @@ where
             );
         };
 
-        let (payload, fb_payload) = build_block(
-            &mut state,
-            &ctx,
-            &mut info,
-            !disable_state_root || ctx.attributes().no_tx_pool, // need to calculate state root for CL sync
-        )?;
-
-        self.payload_tx
-            .send(payload.clone())
+        // We should always calculate state root for fallback payload
+        let (fallback_payload, fb_payload) = build_block(&mut state, &ctx, &mut info, true)?;
+        self.built_fb_payload_tx
+            .send(fallback_payload.clone())
             .await
             .map_err(PayloadBuilderError::other)?;
-        best_payload.set(payload);
+        let mut best_payload = fallback_payload.clone();
 
         info!(
             target: "payload_builder",
@@ -529,6 +529,15 @@ where
             let _entered = fb_span.enter();
 
             if ctx.flashblock_index() > ctx.target_flashblock_count() {
+                self.resolve_best_payload(
+                    &mut state,
+                    &ctx,
+                    &mut info,
+                    best_payload,
+                    fallback_payload,
+                    &resolve_payload,
+                )
+                .await;
                 self.record_flashblocks_metrics(
                     &ctx,
                     &info,
@@ -548,15 +557,22 @@ where
                     &state_provider,
                     &mut best_txs,
                     &block_cancel,
-                    &best_payload,
+                    &mut best_payload,
                     &fb_span,
                 )
                 .await
             {
                 Ok(Some(next_flashblocks_ctx)) => next_flashblocks_ctx,
                 Ok(None) => {
-                    self.resolve_best_payload(&mut state, &ctx, &mut info, &best_payload)
-                        .await;
+                    self.resolve_best_payload(
+                        &mut state,
+                        &ctx,
+                        &mut info,
+                        best_payload,
+                        fallback_payload,
+                        &resolve_payload,
+                    )
+                    .await;
                     self.record_flashblocks_metrics(
                         &ctx,
                         &info,
@@ -583,7 +599,15 @@ where
                     ctx = ctx.with_cancel(fb_cancel).with_extra_ctx(next_flashblocks_ctx);
                 },
                 _ = block_cancel.cancelled() => {
-                    self.resolve_best_payload(&mut state, &ctx, &mut info, &best_payload).await;
+                    self.resolve_best_payload(
+                        &mut state,
+                        &ctx,
+                        &mut info,
+                        best_payload,
+                        fallback_payload,
+                        &resolve_payload,
+                    )
+                    .await;
                     self.record_flashblocks_metrics(
                         &ctx,
                         &info,
@@ -609,7 +633,7 @@ where
         state_provider: impl reth::providers::StateProvider + Clone,
         best_txs: &mut NextBestFlashblocksTxs<Pool>,
         block_cancel: &CancellationToken,
-        best_payload: &BlockCell<OpBuiltPayload>,
+        best_payload: &mut OpBuiltPayload,
         span: &tracing::Span,
     ) -> eyre::Result<Option<FlashblocksExtraCtx>> {
         let flashblock_index = ctx.flashblock_index();
@@ -769,11 +793,11 @@ where
                     .ws_pub
                     .publish(&fb_payload)
                     .wrap_err("failed to publish flashblock via websocket")?;
-                self.payload_tx
+                self.built_fb_payload_tx
                     .send(new_payload.clone())
                     .await
                     .wrap_err("failed to send built payload to handler")?;
-                best_payload.set(new_payload);
+                *best_payload = new_payload;
 
                 // Record flashblock build duration
                 ctx.metrics
@@ -835,50 +859,75 @@ where
         state: &mut State<DB>,
         ctx: &OpPayloadBuilderCtx<FlashblocksExtraCtx>,
         info: &mut ExecutionInfo<FlashblocksExecutionInfo>,
-        best_payload: &BlockCell<OpBuiltPayload>,
+        best_payload: OpBuiltPayload,
+        fallback_payload: OpBuiltPayload,
+        resolve_payload: &BlockCell<OpBuiltPayload>,
     ) {
-        if let Some(payload) = best_payload.get()
-            && payload.block().header().state_root == B256::ZERO
-        {
-            // Calculate state root for best payload on resolve payload to prevent
-            // cache misses for locally built payloads.
-            if let Ok((state_root, trie_updates)) = calculate_state_root_only(state, ctx, info) {
-                use reth_payload_primitives::BuiltPayload as _;
-                let payload_id = payload.id();
-                let fees = payload.fees();
-                let executed_block =
-                    payload
-                        .executed_block()
-                        .map(|executed_block| BuiltPayloadExecutedBlock {
-                            recovered_block: executed_block.recovered_block.clone(),
-                            execution_output: executed_block.execution_output.clone(),
-                            hashed_state: executed_block.hashed_state.clone(),
-                            trie_updates: Either::Left(Arc::new(trie_updates)),
-                        });
-
-                // Get the current sealed block and extract its components
-                let block = payload.into_sealed_block().into_block();
-                let (mut header, body) = block.split();
-                header.state_root = state_root;
-
-                // Create a new sealed block with the updated header
-                let updated_block =
-                    alloy_consensus::Block::<OpTransactionSigned>::new(header, body);
-                let sealed_block = Arc::new(updated_block.seal_slow());
-
-                // Update the best payload with the calculated staet root
-                let updated_payload =
-                    OpBuiltPayload::new(payload_id, sealed_block, fees, executed_block);
-                self.payload_tx.send(updated_payload.clone()).await.ok(); // ignore error here
-                best_payload.set(updated_payload);
-
-                debug!(
-                    target: "payload_builder",
-                    state_root = %state_root,
-                    "Updated payload with calculated state root"
-                );
-            }
+        if resolve_payload.get().is_some() {
+            return;
         }
+
+        let payload = match best_payload.block().header().state_root {
+            B256::ZERO => {
+                info!(target: "payload_builder", "Resolving payload with zero state root");
+                self.resolve_zero_state_root(state, ctx, info, best_payload, fallback_payload)
+                    .await
+            }
+            _ => best_payload,
+        };
+        resolve_payload.set(payload);
+    }
+
+    async fn resolve_zero_state_root<
+        DB: Database<Error = ProviderError> + std::fmt::Debug + AsRef<P>,
+        P: StateRootProvider + HashedPostStateProvider + StorageRootProvider,
+    >(
+        &self,
+        state: &mut State<DB>,
+        ctx: &OpPayloadBuilderCtx<FlashblocksExtraCtx>,
+        info: &ExecutionInfo<FlashblocksExecutionInfo>,
+        best_payload: OpBuiltPayload,
+        fallback_payload: OpBuiltPayload,
+    ) -> OpBuiltPayload {
+        let Ok((state_root, trie_updates)) = calculate_state_root_only(state, ctx, info) else {
+            // This throws away all previously built fb payloads that has already been broadcasted
+            // and is not ideal. The state root calculation should be bulletproof and not fail
+            // under normal circumstances.
+            return fallback_payload;
+        };
+
+        let payload_id = best_payload.id();
+        let fees = best_payload.fees();
+        let executed_block =
+            best_payload
+                .executed_block()
+                .map(|executed_block| BuiltPayloadExecutedBlock {
+                    recovered_block: executed_block.recovered_block.clone(),
+                    execution_output: executed_block.execution_output.clone(),
+                    hashed_state: executed_block.hashed_state.clone(),
+                    trie_updates: Either::Left(Arc::new(trie_updates)),
+                });
+        let block = best_payload.into_sealed_block().into_block();
+        let (mut header, body) = block.split();
+        header.state_root = state_root;
+        let updated_block = alloy_consensus::Block::<OpTransactionSigned>::new(header, body);
+        let sealed_block = Arc::new(updated_block.seal_slow());
+
+        let updated_payload = OpBuiltPayload::new(payload_id, sealed_block, fees, executed_block);
+        if let Err(e) = self.built_payload_tx.send(updated_payload.clone()).await {
+            warn!(
+                target: "payload_builder",
+                error = %e,
+                "Failed to send updated payload"
+            );
+        }
+        debug!(
+            target: "payload_builder",
+            state_root = %state_root,
+            "Updated payload with calculated state root"
+        );
+
+        updated_payload
     }
 
     /// Do some logging and metric recording when we stop build flashblocks
