cf0c020fadae1008b60803aa5fb497bcbd5dfffc
Niven <sieniven@gmail.com>
2026-02-04 10:38:03 +0800

    Fix to spawn blocking

--- crates/op-rbuilder/src/builders/flashblocks/payload_handler.rs
+++ crates/op-rbuilder/src/builders/flashblocks/payload_handler.rs
@@ -113,7 +113,7 @@ where
 
                             // execute the flashblock on a thread where blocking is acceptable,
                             // as it's potentially a heavy operation
-                            task_executor.spawn(Box::pin(async move {
+                            task_executor.spawn_blocking(Box::pin(async move {
                                 let res = execute_flashblock(
                                     payload,
                                     ctx,
