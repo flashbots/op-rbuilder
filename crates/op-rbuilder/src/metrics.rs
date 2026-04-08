dbc77eee34324231e6e72628232684895232fadb
Ash Kunda <18058966+akundaz@users.noreply.github.com>
2026-04-08 14:44:57 -0400

    remove unused metrics

--- crates/op-rbuilder/src/metrics.rs
+++ crates/op-rbuilder/src/metrics.rs
@@ -147,10 +147,6 @@ pub struct OpRBuilderMetrics {
     pub reduced_flashblocks_number: Counter,
     /// How much less flashblocks we issued in reality, comparing to calculated number for block
     pub missing_flashblocks_count: Counter,
-    /// How much time we have deducted from block building time
-    pub flashblocks_time_drift: Histogram,
-    /// Time offset we used for first flashblock
-    pub first_flashblock_time_offset: Histogram,
     /// Delay in milliseconds between expected and actual FCU arrival time (positive = late, negative = early)
     pub fcu_arrival_delay: Histogram,
     /// Number of requests sent to the eth_sendBundle endpoint
